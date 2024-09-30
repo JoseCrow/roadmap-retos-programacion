514dda652d746a59a5cd86de5f0c7b4c6d3c4e46
JoseCrow <josemanel@outlook.com>
2024-09-30 14:35:31 -0400

    josecrow.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/josecrow.py
@@ -0,0 +1,25 @@
+https://www.python.org/
+"comentario en una linea"
+'comentario en una linea'
+>>> """
+... esto tambien es
+... un comentario
+... en varias lineas
+... """
+'\nesto tambien es \nun comentario \nen varias lineas\n'
+>>> ""
+''
+>>> esto tambien
+  File "<python-input-3>", line 1
+    esto tambien
+         ^^^^^^^
+SyntaxError: invalid syntax
+>>> my_variable: " mi variable"
+>>> my_variable: " esta es mi nueva variable"
+>>>
+my_int = 1
+>>> my_float= 1.5
+>>> my_bool = TRUE
+ my_bool= " FALSE"
+ print ("HOLA JOSECROW")
+HOLA JOSECROW
